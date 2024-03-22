e887f5a8c385d99ca4f121a7a446d0eaacd805a8
From 0 to 1 <101225909+VitaliPri@users.noreply.github.com>
2024-03-22 12:30:34 -0400

    slider structure

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".swiper", {
+const swiperTop = new Swiper(".top__swiper", {
   // Optional parameters
   effect: "fade",
 
@@ -7,6 +7,16 @@ const swiper = new Swiper(".swiper", {
     disableOnInteraction: false,
   },
 
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiperAbout = new Swiper(".about__slider", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
